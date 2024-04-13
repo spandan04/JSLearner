a4444631312ec335a254a54599d72b5d93a5e806
spandan04 <77854717+spandan04@users.noreply.github.com>
2024-04-13 18:21:36 +0530

    Create Month calci.js

---
+++ Month calci.js
@@ -0,0 +1,49 @@
+function month(num){
+    switch(num) {
+        case 1:
+            console.log("jan")
+            break;
+        case 2:
+            console.log("feb")
+        
+            break;
+       case 3:
+            console.log("mar")
+            break;
+        case 4:
+            console.log("april")
+        
+            break;
+        case 5:
+            console.log("may")
+            break;
+        case 6:
+            console.log("june")
+        
+            break;
+        case 7:
+            console.log("july")
+            break;
+        case 8:
+            console.log("aug")
+        
+            break;
+        case 9:
+            console.log("sep")
+            break;
+        case 10:
+            console.log("oct")
+        
+            break;
+        case 11:
+            console.log("nov")
+            break;
+        case 12:
+            console.log("dec")
+        
+            break;
+        default:
+            console.log("bkl saala");
+    }
+}
+month(15)
